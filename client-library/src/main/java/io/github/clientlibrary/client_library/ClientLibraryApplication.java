814af9522d1774abd419e138bda869cbb0f2a3ff
jassonluizjr <jassonluizjr@hotmail.com>
2025-07-24 01:14:00 -0300

    Adds the Client Library project

---
+++ client-library/src/main/java/io/github/clientlibrary/client_library/ClientLibraryApplication.java
@@ -0,0 +1,13 @@
+package io.github.clientlibrary.client_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientLibraryApplication.class, args);
+	}
+
+}
---
+++ client-library/src/test/java/io/github/clientlibrary/client_library/ClientLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.clientlibrary.client_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
